724a22ff69379d1aba3db91edd3f017c5f69d282
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-09-18 18:42:27 +0545

    crypto currency price api

--- crypto/views.py
+++ crypto/views.py
@@ -5,7 +5,12 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
-    api = json.loads(api_request.content)
-    # print(api)
-    return render(request, 'home.html', {'api': api})
+
+    # Grab crypto price data
+    price_api_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,BCH,LTC,USDT,EOS,BNB,BSV,XLM&tsyms=USD")
+    price_api = json.loads(price_api_request.content)
+
+    # Grab crypto news
+    news_api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    news_api = json.loads(news_api_request.content)
+    return render(request, 'home.html', {'news_api': news_api, 'price_api': price_api})
